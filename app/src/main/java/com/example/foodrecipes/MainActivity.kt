9e413345ee85313ad6b4613f351602bdcf048c7b
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-01-10 15:59:07 +0900

    Create : Create Project & Create Main Navigation

---
+++ app/src/main/java/com/example/foodrecipes/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.foodrecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigationController = findNavController(R.id.navigationHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            ))
+
+        bottomNavigationView.setupWithNavController(navigationController)
+        setupActionBarWithNavController(navigationController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigationController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
